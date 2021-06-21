ac632c1346982a3eddfc6f017561fe267fa7de76
James Aymer <aymerjames@gmail.com>
2021-06-21 13:32:50 +0100

    ## Overview
    ### What is the feature?
    This is the initial commit for this project, which looks at the permissions required to take a programmatic screenshot.
    
    ### What is the planned solution?
    The solution was to create the application and test the logic to ensure that everything was working as expected.

---
+++ README.md
@@ -0,0 +1,47 @@
+# Android Screenshot
+
+## About The Project
+
+This Java Android project looked at how to take a screenshot programmatically from within an application.
+
+Both the **READ_EXTERNAL_STORAGE** & **WRITE_EXTERNAL_STORAGE** permissions are requested by the user in the main activity.
+
+Once permission has been granted, the button's on click listener can be invoked to capture the screenshot.
+
+
+## App Screenshots
+
+![Android Permissions App Screenshots](img/sshot-screenshot.png)
+
+
+## Built With
+
+* [Kotlin: 1.5.10](https://kotlinlang.org/)
+* [Android Studio: 4.2](https://developer.android.com/studio)
+* [Android Lollipop: (5.0)](https://www.android.com/intl/en_uk/versions/lollipop-5-0/)
+* [Android 5.0: (API level 21)](https://developer.android.com/about/versions/lollipop)
+
+
+## Prerequisites
+
+* [Kotlin: 1.5.10](https://kotlinlang.org/)
+* [Android Studio: 4.2](https://developer.android.com/studio)
+* [Android Lollipop: (5.0)](https://www.android.com/intl/en_uk/versions/lollipop-5-0/)
+* [Android 5.0: (API level 21)](https://developer.android.com/about/versions/lollipop)
+
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+## Contact
+
+- [Email](mailto:aymerjames@gmail.com)
+- [Linkedin](https://uk.linkedin.com/in/jamesaymer)
+- [Github](https://github.com/JaymoAymer)
+
+
+## Acknowledgements
+* [Android Developer Documentation: Take a screenshot](https://developer.android.com/studio/debug/am-screenshot)
+* [Geek For Geeks: How to take a screenshot programmatically in Android?](https://www.geeksforgeeks.org/how-to-take-screenshot-programmatically-in-android/)
--- app/src/main/java/com/example/androidscreenshot/MainActivity.java
+++ app/src/main/java/com/example/androidscreenshot/MainActivity.java
@@ -1,8 +1,22 @@
 package com.example.androidscreenshot;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verifyStoragePermision(this);
+        Button btnPermissions = findViewById(R.id.btn_permissions);
+
+        btnPermissions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeScreenshot(getWindow().getDecorView().getRootView(), "screenshotResult");
+            }
+        });
+    }
+
+    protected static File takeScreenshot(View view, String fileName) {
+
+        // Saving the image based upon the date
+        String pattern = "yyyy-MM-dd_hh:mm:ss";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+        try {
+            String dirPath = Environment.getExternalStorageDirectory().toString() + "/androidScreenshot";
+            File fileDir = new File(dirPath);
+
+            if (!fileDir.exists()) {
+                boolean mkdir = fileDir.mkdir();
+            }
+
+            String path = dirPath + "/" + fileName + "-" + simpleDateFormat + ".png";
+
+            view.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(path);
+
+            FileOutputStream fileOutputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.PNG, quality, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            return imageFile;
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSION_STORAGE = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    public static void verifyStoragePermision(Activity activity) {
+
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, PERMISSION_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
     }
 }
